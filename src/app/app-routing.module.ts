1d4c5af45262da0cf4a45cddac30cfae2497d083
BalajiM1603 <mbalaji1603@gmail.com>
2019-06-26 17:37:01 +0530

    CleanBlog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SamplepostComponent } from './samplepost/samplepost.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:HomeComponent
+  },
+  {
+    path:"about",
+    component:AboutComponent
+  },
+  {
+    path:"samplepost",
+    component:SamplepostComponent
+    },
+    {
+      path:"contact",
+      component:ContactComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SamplepostComponent } from './samplepost/samplepost.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    
+    
+    
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    SamplepostComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
